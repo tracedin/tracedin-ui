05934a8fa7b8e8695bb06ec4e1ee3e23d169e3f6
sukkyun2 <goobghd@gmail.com>
2024-09-30 12:26:03 +0900

    chore: import alias, svg plugin 추가

---
+++ src/types/svg.d.ts
@@ -0,0 +1,5 @@
+// svg.d.ts
+declare module '*.svg' {
+  const content: React.FC<React.SVGProps<SVGElement>>;
+  export default content;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx"],
+  },
 })
